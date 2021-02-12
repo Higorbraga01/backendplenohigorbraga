e52d309496425692f7db228706022666e006dfc5
Higor Braga <higorbraga00@gmail.com>
2021-02-12 18:57:15 -0300

    Projeto criado

---
+++ src/main/java/com/mobicare/backendplenohigorbraga/BackEndPlenoHigorBragaApplication.java
@@ -0,0 +1,13 @@
+package com.mobicare.backendplenohigorbraga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndPlenoHigorBragaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndPlenoHigorBragaApplication.class, args);
+	}
+
+}
